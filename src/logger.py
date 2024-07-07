fdae3ae5f908f0b6a107efff21ebdaae5dc15d49
Goyalsidd <2022meb1346@gmail.com>
2024-07-07 19:57:53 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,28 @@
+import logging
+import os
+from datetime import datetime
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+if not os.path.exists(logs_path):
+    os.makedirs(logs_path)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# if __name__=="__main__":
+#     logging.info("Logging has started")
+
+# # Example logging messages
+# logger = logging.getLogger(__name__)
+# logger.debug("This is a debug message")
+# logger.info("This is an info message")
+# logger.warning("This is a warning message")
+# logger.error("This is an error message")
+# logger.critical("This is a critical message")
